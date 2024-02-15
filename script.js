0a8ae818b5eb75462058e63759887485a3759dec
Laura Dumitru <lauradumitru88@icloud.com>
2024-02-15 12:49:12 +0000

    Changed element names

--- script.js
+++ script.js
@@ -12,34 +12,52 @@ function handleMove(event) {
   console.log(x);
 }*/
 
-const drag = document.querySelector(".drag");
 const sidebar = document.querySelector(".sidebar");
-const images = document.querySelectorAll(".image");
-const icon = drag.querySelector(".arrow");
-let isDragging = false; // Flag to indicate whether dragging is active
+const content = document.querySelector(".content");
+//const content = sidebar.content
 
-drag.addEventListener("pointerdown", startDrag);
+const images = document.querySelectorAll(".image");
+const icon = sidebar.querySelector(".arrow");
+//let isDragging = false; // Flag to indicate whether dragging is active
+
+sidebar.addEventListener("pointerdown", startDrag);
 document.addEventListener("pointerup", stopDrag);
 
 function startDrag(event) {
-  isDragging = true; // Set dragging flag to true when drag starts
+  //isDragging = true; // Set dragging flag to true when drag starts
   document.addEventListener("pointermove", handleMove);
 }
 
 function stopDrag() {
-  isDragging = false; // Set dragging flag to false when drag stops
+  //isDragging = false; // Set dragging flag to false when drag stops
   document.removeEventListener("pointermove", handleMove);
 }
 
 function handleMove(event) {
-  if (!isDragging) return; // Exit if dragging is not active
+  event.preventDefault();
+  //if (!isDragging) return; // Exit if dragging is not active
+  //const width = sidebar.offsetWidth; // Get the width of the sidebar container
 
-  const width = sidebar.offsetWidth; // Get the width of the sidebar container
-  drag.style.width = width + "px"; // Set the width of the drag element to fit the sidebar
-  images.forEach((image) => {
-    image.style.opacity = "1";
-    icon.style.opacity = "1";
-    icon.style.position = "relative";
-    icon.style.top = "50px";
-  });
+  const width = sidebar.clientWidth;
+
+  const x = event.clientX; //offsetX
+
+  console.log(width); //sidebar.offsetWidth
+
+  //if (x > 0 && x < sidebar.clientWidth)
+  sidebar.style.transform = `translateX(calc(${x}px - 100%))`; //calc()
+
+  //sidebar.style.width = "50%";
+
+  //content.style.visibility = "visible";
 }
+
+/*
+if (x > sidebarDrag.size.width && x <= width) {
+  // 390 (is the right edge of the screen)
+  sidebar.style.transform = `translateX(${x - width}px)`; // translateX refers by default to the left edge of the gallery
+
+  sidebarOpened = true;
+  mediator("Sidebar open");
+}
+*/

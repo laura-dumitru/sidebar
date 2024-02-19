2fef8a39675e8e804b627d675be94cf51d59614b
Laura Dumitru <lauradumitru88@icloud.com>
2024-02-19 17:35:48 +0000

    Added logic to deal with chevron arrows animation

--- script.js
+++ script.js
@@ -5,16 +5,35 @@ const content = document.querySelector(".content");
 const images = document.querySelectorAll(".image");
 const icon = sidebar.querySelector(".arrow");
 const pulseArrow = document.querySelector(".right");
+const arrowUp = document.querySelector(".arrow-up");
+const arrowDown = document.querySelector(".arrow-down");
+const chevronsRight = document.querySelector("[data-lucide='chevrons-right']");
 
 //let isDragging = false; // Flag to indicate whether dragging is active
 
+let visibilityPercentage;
+
 sidebar.addEventListener("pointerdown", startDrag);
-document.addEventListener("pointerup", stopDrag);
+document.addEventListener("pointerup", (event) => {
+  if (visibilityPercentage > 99) {
+    chevronsRight.setAttribute("data-lucide", "chevrons-left");
+    lucide.createIcons();
+    animation();
+  } else if (visibilityPercentage <= 0) {
+    console.log("I am zero");
+    // chevronsRight.style.display = "none";
+    chevronsRight.setAttribute("data-lucide", "chevrons-right");
+
+    console.log(chevronsRight.getAttribute("data-lucide"));
+    lucide.createIcons();
+  }
+  stopDrag(event);
+});
 
 function animation() {
   gsap.to(".right", {
-    x: "+=5", // Increase position to the right by 10 units
-    repeat: 4, // Repeat indefinitely
+    x: "+=4", // Increase position to the right by 10 units
+    repeat: 3, // Repeat indefinitely
     yoyo: true, // Reverse the animation to return to the initial position
     ease: "power1.inOut", // Use a smooth easing function
     duration: 0.3, // Duration of each animation cycle
@@ -39,8 +58,8 @@ function handleMove(event) {
   const x = event.clientX;
 
   // Calculate the percentage of sidebar visibility based on pointer position
-  const visibilityPercentage = Math.max(0, Math.min(100, ((x - width) / width) * 100));
-
+  visibilityPercentage = Math.max(0, Math.min(100, ((x - width) / width) * 100));
+  console.log(visibilityPercentage);
   // Calculate the translateX value to move the sidebar
   const translateValue = `${-85 + (visibilityPercentage / 100) * 85}%`;
   // Set the sidebar's position accordingly
@@ -53,7 +72,12 @@ function handleMove(event) {
     content.style.visibility = "hidden";
   }
 }
-
+arrowUp.addEventListener("click", function () {
+  console.log("Click up");
+});
+arrowDown.addEventListener("click", function () {
+  console.log("Click down");
+});
 /*
 if (x > sidebarDrag.size.width && x <= width) {
   // 390 (is the right edge of the screen)

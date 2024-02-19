fde6f32e906f375e761bc23fc04a37d191212c15
Laura Dumitru <lauradumitru88@icloud.com>
2024-02-19 11:54:47 +0000

    Added chevron right animation and fixed sidebar

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ const sidebar = document.querySelector(".sidebar");
 const content = document.querySelector(".content");
 const images = document.querySelectorAll(".image");
 const icon = sidebar.querySelector(".arrow");
+const pulseArrow = document.querySelector(".right");
+
 //let isDragging = false; // Flag to indicate whether dragging is active
 
 sidebar.addEventListener("pointerdown", startDrag);
 document.addEventListener("pointerup", stopDrag);
+
+function animation() {
+  gsap.to(".right", {
+    x: "+=5", // Increase position to the right by 10 units
+    repeat: 4, // Repeat indefinitely
+    yoyo: true, // Reverse the animation to return to the initial position
+    ease: "power1.inOut", // Use a smooth easing function
+    duration: 0.3, // Duration of each animation cycle
+  });
+}
+animation();
 
 function startDrag(event) {
   //isDragging = true; // Set dragging flag to true when drag starts
@@ -21,20 +34,24 @@ function stopDrag() {
 
 function handleMove(event) {
   event.preventDefault();
-  //const width = sidebar.offsetWidth; // Get the width of the sidebar container
 
   const width = sidebar.clientWidth;
+  const x = event.clientX;
 
-  const x = event.clientX; //offsetX
+  // Calculate the percentage of sidebar visibility based on pointer position
+  const visibilityPercentage = Math.max(0, Math.min(100, ((x - width) / width) * 100));
 
-  console.log(width); //sidebar.offsetWidth
+  // Calculate the translateX value to move the sidebar
+  const translateValue = `${-85 + (visibilityPercentage / 100) * 85}%`;
+  // Set the sidebar's position accordingly
+  sidebar.style.transform = `translateX(${translateValue})`;
 
-  //if (x > 0 && x < sidebar.clientWidth)
-  sidebar.style.transform = `translateX(calc(${x}px - 100%))`; //calc()
-
-  //sidebar.style.width = "50%";
-
-  content.style.visibility = "visible";
+  // Adjust content visibility based on sidebar visibility
+  if (visibilityPercentage > 0) {
+    content.style.visibility = "visible";
+  } else {
+    content.style.visibility = "hidden";
+  }
 }
 
 /*
